3c74af33d07e8e7ebd9551dbcae9ef99a30f8438
akshata2005 <akshatasawant1805@gmail.com>
2022-02-14 22:00:28 +0530

    [Akshata] Add: Regex program

--- AlgorithmProgram/AlgorithmProgram/Program.cs
+++ AlgorithmProgram/AlgorithmProgram/Program.cs
@@ -11,12 +11,36 @@ namespace AlgorithmProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Algorithmic Programs");
+            Console.WriteLine("1.BinarySearch\n2.InsertionSort\n3.BubbleSort\n4.PrimeNumbers\n5.Anagram\n6.CustomizeMessage\n7.Exit");
+            Console.Write("Enter the Choice : ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            switch (choice)
+            {
+                case 1:
+                    BinarySearch.Search();
+                    break;
+                case 2:
+                    InsertionSort.Insertionsort();
+                    break;
+                case 3:
+                    Bubblesort.Bubble();
+                    break;
+                case 4:
+                    PrimeNumber.Prime();
+                    break;
+                case 5:
+                    Anagram.FindAnagram();
 
-            BinarySearch.Search();
-            InsertionSort.Insertionsort();
-            Bubblesort.Bubble();
-            PrimeNumber.Prime();
-            Anagram.FindAnagram();
+                    break;
+                case 6:
+                    RegexInput regexp = new RegexInput();
+                    regexp.Check();
+                    break;
+                default:
+                    Console.WriteLine("Thank You... :)");
+                    break;
+            }
         }
     }
 }
---
+++ AlgorithmProgram/AlgorithmProgram/RegexInput.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AlgorithmProgram
+{
+    public class RegexInput
+    {
+        /// <summary>
+        /// Check() method to check the Names and other inputs by using Regex...
+        /// </summary>
+        public void Check()
+        {
+            Console.Write("Enter Your FirstName : ");
+            string fname = Console.ReadLine();
+            Console.Write("Enter Your LastName : ");
+            string lname = Console.ReadLine();
+            string namepattrn = @"^[A-Z]{1}[a-z]{3,}";
+            Regex regex = new Regex(namepattrn);
+            bool firstname = regex.IsMatch(fname);
+            bool lasttname = regex.IsMatch(lname);
+            Console.Write("Enter the Phone No. with Country Code : ");
+            string PhoneNo = Console.ReadLine();
+            string numpattern = @"^[+]{0,1}[0-9]{2}" + " " + "[6-9]{1}[0-9]{9}$";
+            Regex reg = new Regex(numpattern);
+            bool phoneno = reg.IsMatch(PhoneNo);
+            Console.WriteLine("FirstName : " + firstname + "\nLastName : " + lasttname + "\nPhone No. : " + phoneno);
+            Console.Write("Date is : ");
+            DateTime today = DateTime.Now;
+            Console.WriteLine(today.ToString("dd/MM/yyyy"));
+            Console.WriteLine();
+
+            Console.WriteLine("Hello {0}, We have your fullname as {1} {2} in our system.", fname, fname, lname);
+            Console.WriteLine("Your contact number is {0}.", PhoneNo);
+            Console.WriteLine("Please,let us know in case of any clarification Thank you BridgeLabz {0}", today.ToString("dd/MM/yyyy"));
+            Console.ReadKey();
+        }
+    }
+}
