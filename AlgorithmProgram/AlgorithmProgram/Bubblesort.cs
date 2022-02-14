8dc5e8520e9776aebca4689e4dfe08eaa7576d1f
akshata2005 <akshatasawant1805@gmail.com>
2022-02-14 21:32:53 +0530

    [Akshata] Add: BubbleSort sort algorithm for sorting elemnts

---
+++ AlgorithmProgram/AlgorithmProgram/Bubblesort.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProgram
+{
+    public class Bubblesort
+    {
+        /// <summary>
+        /// Bubble() method to sort the given integer array(list of numbers)
+        /// </summary>
+        public static void Bubble()
+        {
+            int[] number = { 50, 12, 87, 369, 1, 446, 99, 62 };
+            bool flag = true;
+            int temp;
+            int numLength = number.Length;
+            //sorting an array  
+            for (int i = 1; (i <= (numLength - 1)) && flag; i++)
+            {
+                flag = false;
+                for (int j = 0; j < (numLength - 1); j++)
+                {
+                    if (number[j + 1] < number[j])
+                    {
+                        //Swapping the numbers
+                        temp = number[j];
+                        number[j] = number[j + 1];
+                        number[j + 1] = temp;
+                        //for going next iteration making flag true
+                        flag = true;
+                    }
+                }
+            }
+            //Print the Sorted array  
+            foreach (int num in number)
+            {
+                Console.Write("  " + num);
+            }
+        }
+    }
+}
--- AlgorithmProgram/AlgorithmProgram/Program.cs
+++ AlgorithmProgram/AlgorithmProgram/Program.cs
@@ -14,6 +14,7 @@ namespace AlgorithmProgram
 
             BinarySearch.Search();
             InsertionSort.Insertionsort();
+            Bubblesort.Bubble();
         }
     }
 }
